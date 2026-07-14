ff00786a7c815f95127f563220693c943fc9331f
sarosarventhiran <sarosarventhiran@gmail.com>
2026-07-14 11:28:26 +0530

    prg3

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // Arrow function to check odd or even
 const isOddOrEven = (num) => {
-    return 
+    if (num % 2 === 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    } 
 };
 
 // Function to display the result
 function checkNumber() {
     const number = Number(document.getElementById("number").value);
-
-    
+    const result = isOddOrEven(number);
+    document.getElementById("result").innerText = `The number ${number} is ${result}.`;   
 }
